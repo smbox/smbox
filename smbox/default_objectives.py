88fcb17022e18ad5fb98940fe0420e1aa3f14faf
tarek-salhi <tarek.salhi@vodafone.com>
2023-11-11 13:27:04 +0000

    improved time budget cut off

--- smbox/default_objectives.py
+++ smbox/default_objectives.py
@@ -1,7 +1,10 @@
 from sklearn.model_selection import cross_validate
+import time
+from sklearn.model_selection import KFold
+from sklearn.base import clone
+from sklearn.metrics import check_scoring
 
-
-def cross_val_performance(classifier, cfg, data, scoring='roc_auc', cv=4):
+def cross_val_performance_legacy(classifier, cfg, data, scoring='roc_auc', cv=4):
     """Evaluate model performance using cross-validation.
 
     Args:
@@ -26,8 +29,48 @@ def cross_val_performance(classifier, cfg, data, scoring='roc_auc', cv=4):
 
     return cv_results['test_score'].mean()
 
+def cross_val_performance(classifier, cfg, data, scoring='roc_auc', cv=4, time_limit=None):
+    """Evaluate model performance using cross-validation with a time check between each fold.
 
-def rf_objective(cfg, data):
+    Args:
+        classifier (object): A classifier object that supports the fit method.
+        cfg (dict): Hyperparameter configuration for the classifier.
+        data (dict): Dataset split into train and test sets.
+        scoring (str, optional): Scoring method for cross-validation. Defaults to 'roc_auc'.
+        cv (int, optional): Number of cross-validation folds. Defaults to 4.
+        time_limit (int, optional): Time limit in seconds for the whole cross-validation process.
+
+    Returns:
+        float: Mean test score from cross-validation or None if the time limit is exceeded.
+    """
+    X_train = data['X_train']
+    y_train = data['y_train']
+    kf = KFold(n_splits=cv)
+    scores = []
+    scorer = check_scoring(classifier(), scoring=scoring)
+    time_status = 'OK'  # Initialize time status
+    start_time = time.time()
+    #print(f'time_limit: {time_limit}', f'end_time: {start_time + time_limit}', f'current_time: {time.time()}')
+
+    for train_index, test_index in kf.split(X_train):
+        if time.time() > start_time + time_limit:
+            time_status = 'END'
+            break  # Time limit exceeded, break out of the loop
+
+        X_train_fold, X_test_fold = X_train.iloc[train_index], X_train.iloc[test_index]
+        y_train_fold, y_test_fold = y_train.iloc[train_index], y_train.iloc[test_index]
+
+        model = clone(classifier(**cfg, random_state=42))
+        model.fit(X_train_fold, y_train_fold)
+        score = scorer(model, X_test_fold, y_test_fold)
+        scores.append(score)
+
+    if time_status == 'END':
+        return 0, time_status  # Return 0 if time limit was exceeded
+    else:
+        return sum(scores) / len(scores), time_status  # Otherwise, return the average score
+
+def rf_objective(cfg, data, time_limit=None):
     """Evaluate the fitness of each solution of the population using RandomForest.
 
     Args:
@@ -39,10 +82,10 @@ def rf_objective(cfg, data):
     """
     from sklearn.ensemble import RandomForestClassifier
 
-    return cross_val_performance(RandomForestClassifier, cfg, data)
+    return cross_val_performance(RandomForestClassifier, cfg, data, time_limit=time_limit)
 
 
-def xgb_objective(cfg, data):
+def xgb_objective(cfg, data, time_limit=None):
     """Evaluate the fitness of each solution of the population using XGBoost.
 
     Args:
@@ -54,4 +97,4 @@ def xgb_objective(cfg, data):
     """
     from xgboost import XGBClassifier
 
-    return cross_val_performance(XGBClassifier, cfg, data)
+    return cross_val_performance(XGBClassifier, cfg, data, time_limit=time_limit)
